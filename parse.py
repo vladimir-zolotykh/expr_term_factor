459290b2e8406fa8a14df354432c3c2c3d3af86b
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-23 11:19:31 +0300

    Initial import
    Parser class

---
+++ parse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+
+
+class Parser:
+    def parse(self):
+        pass
+
+    def expr(self):
+        pass
+
+    def term(self):
+        pass
+
+    def factor(self):
+        pass
